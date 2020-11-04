f1d78e0bd26fa9f75fc8a516b8ab4a003392a77c
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-11-04 15:08:29 -0600

    Initial commit.

---
+++ src/main/java/com/james2ch9developer/my_first_dynamodb/MyFirstDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.james2ch9developer.my_first_dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstDynamodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/james2ch9developer/my_first_dynamodb/MyFirstDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james2ch9developer.my_first_dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
