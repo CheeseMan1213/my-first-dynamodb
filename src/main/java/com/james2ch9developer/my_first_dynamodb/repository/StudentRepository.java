94ba790c390373fbb3859fa2055ed056476817f0
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-11-05 15:06:54 -0600

    I added the 'derjust' dependency called 'spring-data-dynamodb' and I have done the initial commit of some files.

---
+++ src/main/java/com/james2ch9developer/my_first_dynamodb/repository/StudentRepository.java
@@ -0,0 +1,2 @@
+package com.james2ch9developer.my_first_dynamodb.repository;public class StudentRepository {
+}
---
+++ src/main/java/com/james2ch9developer/my_first_dynamodb/rest_controller/StudentController.java
@@ -0,0 +1,2 @@
+package com.james2ch9developer.my_first_dynamodb.rest_controller;public class StudentController {
+}
